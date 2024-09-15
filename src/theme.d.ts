1ae8c5ec7f5ab00124478a0e505cb503f0b087c3
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-15 21:14:43 +0300

    Расширяет интерфейс палитры

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -1,6 +1,23 @@
 import '@mui/material/styles';
+
 declare module '@mui/material/styles' {
+  interface PaletteColor {
+    shades?: {
+      [key: number]: string;
+    };
+  }
+
+  interface Palette {
+    primary: PaletteColor & {
+      shades?: {
+        [key: number]: string;
+      };
+    };
+  }
+
   interface Theme {
+    palette?: PaletteOptions;
+
     customVariables: {
       borderRadiusMain: number;
       borderRadiusSpan: number;
