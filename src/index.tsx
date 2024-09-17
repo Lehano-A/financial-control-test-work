5cc073f918aaa781a6e3f4314e0999303bd59758
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-17 07:54:59 +0300

    Переносит папку theme на верхний уровень

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme, { globalStyles } from './components/theme/theme';
+import theme, { globalStyles } from './theme/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
