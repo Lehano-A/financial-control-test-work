7a12037863ceddce68409023cc01c01ec01709f0
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-09-29 14:27:37 +0300

    Добавляет хук взаимодействия данных товаров с локальным хранилищем

---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+const keyStorageDataProducts = 'dataProducts';
+
+export { keyStorageDataProducts };
---
+++ src/hooks/useLocalStorageDataTable.tsx
@@ -0,0 +1,27 @@
+import { Data } from '../data/types/dataProduct.types';
+
+function useLocalStorageDataTable() {
+  return {
+    get(key: string) {
+      const data = localStorage.getItem(key);
+
+      if (data) {
+        return JSON.parse(data);
+      }
+    },
+
+    add(key: string, data: Data[]) {
+      localStorage.setItem(key, JSON.stringify(data));
+    },
+
+    checkFor(key: string) {
+      return localStorage.getItem(key);
+    },
+
+    update(key: string, data: Data[]) {
+      this.add(key, data);
+    },
+  };
+}
+
+export default useLocalStorageDataTable;
