7da40637151268a491060833d78dd63f837ab842
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-05 17:09:18 +0300

    Создаёт функцию сортировки данных продуктов по умолчанию

---
+++ src/helpers/getSortedDataProductsByDefault.ts
@@ -0,0 +1,13 @@
+import { DataProducts } from '../components/TableProducts/types/dataProducts.types';
+import { Data } from '../data/types/dataProduct.types';
+import compareValues from './compareValues';
+
+function getSortedDataByDefault(dataProducts: DataProducts) {
+  if (dataProducts) {
+    return [...dataProducts].sort((a: Data, b: Data) => {
+      return compareValues(a.id, b.id);
+    });
+  }
+}
+
+export default getSortedDataByDefault;
