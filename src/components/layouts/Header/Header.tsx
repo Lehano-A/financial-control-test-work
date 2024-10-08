93c20b42be00b62d1029bd037b9b54409d7ec697
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-08 18:47:18 +0300

    Правит файловую структуру Header

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material';
 
-import { HeaderProps } from './Header.types';
+import { HeaderProps } from './types/header.types';
 
 const StyledHeader = styled('header')(() => ({
   maxWidth: '750px',
